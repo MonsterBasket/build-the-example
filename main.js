0b99f6b5c4e7e51889002249f532ba02767051ae
MonsterBasket <blazza1@hotmail.com>
2022-07-25 16:21:43 +1000

    test complete

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
+let errorMessage = document.querySelector('#modal');
+errorMessage.className = "hidden";
 
+for (const heart of document.querySelectorAll(".like-glyph")){
+  heart.addEventListener('click', like);
+}
+
+function like(event){
+  mimicServerCall()
+    .then((a) => {
+  const heart = event.target;
+  if (heart.className === "like-glyph"){
+    heart.innerText = FULL_HEART;
+    heart.className = "activated-heart"
+  }
+  else{
+    heart.innerText = EMPTY_HEART;
+    heart.className = "like-glyph";
+  }
+  console.log(event.target, "clicked");})
+      .catch((object) => {
+        errorMessage.className = "";
+        document.querySelector("#modal-message").innerHTML = (JSON.stringify(object))
+        setTimeout(a => errorMessage.className = "hidden", 5000)
+      })
+}
 
 
 
